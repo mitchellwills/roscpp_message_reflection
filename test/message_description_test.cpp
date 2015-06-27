fd07769d8efb72259c1a7684d9c5937f5435fda9
Mitchell Wills <mwills@wpi.edu>
2015-06-27 07:51:13 -0400

    Fixed tests between distros

--- test/message_description_test.cpp
+++ test/message_description_test.cpp
@@ -3,6 +3,9 @@
 #include <boost/scoped_ptr.hpp>
 #include <roscpp_message_reflection/message_description_provider.h>
 
+// NOTE Since comments in message full text varies between distros (even with same hash)
+//      we just check that the full text is not empty
+
 using namespace roscpp_message_reflection;
 
 #define EXPECT_VALUE_FIELD(expected_name, expected_type, field)	\
@@ -58,14 +61,14 @@ TEST(MessageDescription, construct_simple_message)
   fields.push_back(FieldDescription::CreateFromFullType("a", "int32"));
   fields.push_back(FieldDescription::CreateFromFullType("b", "uint16"));
   fields.push_back(FieldDescription::CreateFromFullType("d", "string"));
-  MessageDescription message("a_package/AMessage", "992ce8a1687cec8c8bd883ec73ca1234", "int32 a\nuint16 b\nstring d\n\n", fields, std::map<std::string, MessageDescription::Ptr>());
-  EXPECT_EQ("a_package/AMessage", message.name);
-  EXPECT_EQ("992ce8a1687cec8c8bd883ec73ca1234", message.md5sum);
-  EXPECT_EQ("int32 a\nuint16 b\nstring d\n\n", message.full_text);
-  ASSERT_EQ(3, message.fields.size());
-  EXPECT_VALUE_FIELD("a", "int32", message.fields[0]);
-  EXPECT_VALUE_FIELD("b", "uint16", message.fields[1]);
-  EXPECT_VALUE_FIELD("d", "string", message.fields[2]);
+  MessageDescription description("a_package/AMessage", "992ce8a1687cec8c8bd883ec73ca1234", "int32 a\nuint16 b\nstring d\n\n", fields, std::map<std::string, MessageDescription::Ptr>());
+  EXPECT_EQ("a_package/AMessage", description.name);
+  EXPECT_EQ("992ce8a1687cec8c8bd883ec73ca1234", description.md5sum);
+  EXPECT_GT(description.full_text.size(), 0);
+  ASSERT_EQ(3, description.fields.size());
+  EXPECT_VALUE_FIELD("a", "int32", description.fields[0]);
+  EXPECT_VALUE_FIELD("b", "uint16", description.fields[1]);
+  EXPECT_VALUE_FIELD("d", "string", description.fields[2]);
 }
 
 
@@ -85,7 +88,7 @@ TEST_F(MessageDescriptionProviderTest, get_std_msgs_String_description)
   ASSERT_TRUE(description);
   EXPECT_EQ("std_msgs/String", description->name);
   EXPECT_EQ("992ce8a1687cec8c8bd883ec73ca41d1", description->md5sum);
-  EXPECT_EQ("string data\n\n", description->full_text);
+  EXPECT_GT(description->full_text.size(), 0);
 
   ASSERT_EQ(1, description->fields.size());
   EXPECT_VALUE_FIELD("data", "string", description->fields[0]);
@@ -112,7 +115,7 @@ TEST_F(MessageDescriptionProviderTest, get_geometry_msgs_PoseWithCovariance_desc
   ASSERT_TRUE(description);
   EXPECT_EQ("geometry_msgs/PoseWithCovariance", description->name);
   EXPECT_EQ("c23e848cf1b7533a8d7c259073a97e6f", description->md5sum);
-  EXPECT_EQ("# This represents a pose in free space with uncertainty.\n\nPose pose\n\n# Row-major representation of the 6x6 covariance matrix\n# The orientation parameters use a fixed-axis representation.\n# In order, the parameters are:\n# (x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis)\nfloat64[36] covariance\n\n================================================================================\nMSG: geometry_msgs/Pose\n# A representation of pose in free space, composed of postion and orientation. \nPoint position\nQuaternion orientation\n\n================================================================================\nMSG: geometry_msgs/Point\n# This contains the position of a point in free space\nfloat64 x\nfloat64 y\nfloat64 z\n\n================================================================================\nMSG: geometry_msgs/Quaternion\n# This represents an orientation in free space in quaternion form.\n\nfloat64 x\nfloat64 y\nfloat64 z\nfloat64 w\n\n", description->full_text);
+  EXPECT_GT(description->full_text.size(), 0);
 
   ASSERT_EQ(2, description->fields.size());
   EXPECT_VALUE_FIELD("pose", "geometry_msgs/Pose", description->fields[0]);
@@ -126,7 +129,7 @@ TEST_F(MessageDescriptionProviderTest, get_geometry_msgs_PoseWithCovarianceStamp
   ASSERT_TRUE(description);
   EXPECT_EQ("geometry_msgs/PoseWithCovarianceStamped", description->name);
   EXPECT_EQ("953b798c0f514ff060a53a3498ce6246", description->md5sum);
-  EXPECT_EQ("# This expresses an estimated pose with a reference coordinate frame and timestamp\n\nHeader header\nPoseWithCovariance pose\n\n================================================================================\nMSG: std_msgs/Header\n# Standard metadata for higher-level stamped data types.\n# This is generally used to communicate timestamped data \n# in a particular coordinate frame.\n# \n# sequence ID: consecutively increasing ID \nuint32 seq\n#Two-integer timestamp that is expressed as:\n# * stamp.sec: seconds (stamp_secs) since epoch (in Python the variable is called 'secs')\n# * stamp.nsec: nanoseconds since stamp_secs (in Python the variable is called 'nsecs')\n# time-handling sugar is provided by the client library\ntime stamp\n#Frame this data is associated with\n# 0: no frame\n# 1: global frame\nstring frame_id\n\n================================================================================\nMSG: geometry_msgs/PoseWithCovariance\n# This represents a pose in free space with uncertainty.\n\nPose pose\n\n# Row-major representation of the 6x6 covariance matrix\n# The orientation parameters use a fixed-axis representation.\n# In order, the parameters are:\n# (x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis)\nfloat64[36] covariance\n\n================================================================================\nMSG: geometry_msgs/Pose\n# A representation of pose in free space, composed of postion and orientation. \nPoint position\nQuaternion orientation\n\n================================================================================\nMSG: geometry_msgs/Point\n# This contains the position of a point in free space\nfloat64 x\nfloat64 y\nfloat64 z\n\n================================================================================\nMSG: geometry_msgs/Quaternion\n# This represents an orientation in free space in quaternion form.\n\nfloat64 x\nfloat64 y\nfloat64 z\nfloat64 w\n\n", description->full_text);
+  EXPECT_GT(description->full_text.size(), 0);
 
   ASSERT_EQ(2, description->fields.size());
   EXPECT_VALUE_FIELD("header", "std_msgs/Header", description->fields[0]);
@@ -141,7 +144,7 @@ TEST_F(MessageDescriptionProviderTest, get_geometry_msgs_PoseArray_description)
   ASSERT_TRUE(description);
   EXPECT_EQ("geometry_msgs/PoseArray", description->name);
   EXPECT_EQ("916c28c5764443f268b296bb671b9d97", description->md5sum);
-  EXPECT_EQ("# An array of poses with a header for global reference.\n\nHeader header\n\nPose[] poses\n\n================================================================================\nMSG: std_msgs/Header\n# Standard metadata for higher-level stamped data types.\n# This is generally used to communicate timestamped data \n# in a particular coordinate frame.\n# \n# sequence ID: consecutively increasing ID \nuint32 seq\n#Two-integer timestamp that is expressed as:\n# * stamp.sec: seconds (stamp_secs) since epoch (in Python the variable is called 'secs')\n# * stamp.nsec: nanoseconds since stamp_secs (in Python the variable is called 'nsecs')\n# time-handling sugar is provided by the client library\ntime stamp\n#Frame this data is associated with\n# 0: no frame\n# 1: global frame\nstring frame_id\n\n================================================================================\nMSG: geometry_msgs/Pose\n# A representation of pose in free space, composed of postion and orientation. \nPoint position\nQuaternion orientation\n\n================================================================================\nMSG: geometry_msgs/Point\n# This contains the position of a point in free space\nfloat64 x\nfloat64 y\nfloat64 z\n\n================================================================================\nMSG: geometry_msgs/Quaternion\n# This represents an orientation in free space in quaternion form.\n\nfloat64 x\nfloat64 y\nfloat64 z\nfloat64 w\n\n", description->full_text);
+  EXPECT_GT(description->full_text.size(), 0);
 
   ASSERT_EQ(2, description->fields.size());
   EXPECT_VALUE_FIELD("header", "std_msgs/Header", description->fields[0]);
